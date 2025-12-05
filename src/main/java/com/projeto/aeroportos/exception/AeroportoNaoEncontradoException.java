ac1ccf70045e7474ac4ca02e2e006b710733e7ac
DanielBonypucminas <danielbony2105@gmail.com>
2025-12-05 16:53:17 -0300

    feat: configuração da exceção lançada para aeroporto nao encontrado

--- src/main/java/com/projeto/aeroportos/exception/AeroportoNaoEncontradoException.java
+++ src/main/java/com/projeto/aeroportos/exception/AeroportoNaoEncontradoException.java
@@ -1,5 +1,16 @@
 package com.projeto.aeroportos.exception;
 
-public class AeroportoNaoEncontradoException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class AeroportoNaoEncontradoException extends RuntimeException {
     
+    public AeroportoNaoEncontradoException(String codigoIata) {
+        super("Aeroporto com código IATA '" + codigoIata + "' não encontrado.");
+    }
+    
+    public AeroportoNaoEncontradoException(Long id) {
+        super("Aeroporto com ID '" + id + "' não encontrado.");
+    }
 }
