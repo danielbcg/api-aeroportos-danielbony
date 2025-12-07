1f5e409571802855b00302f1134237819596dca3
DanielBonypucminas <danielbony2105@gmail.com>
2025-12-07 15:18:34 -0300

    feat: adiciona configurações e scripts do banco

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+-- População inicial da tabela aeroporto
+INSERT INTO aeroporto (nome_aeroporto, codigo_iata, cidade, codigo_pais_iso, latitude, longitude, altitude) VALUES
+('Aeroporto Internacional de São Paulo/Guarulhos', 'GRU', 'São Paulo', 'BR', -23.4356, -46.4731, 750.0),
+('Aeroporto de Congonhas', 'CGH', 'São Paulo', 'BR', -23.6261, -46.6564, 802.0),
+('Aeroporto Internacional do Rio de Janeiro/Galeão', 'GIG', 'Rio de Janeiro', 'BR', -22.8089, -43.2436, 9.0),
+('Aeroporto Santos Dumont', 'SDU', 'Rio de Janeiro', 'BR', -22.9103, -43.1631, 3.0),
+('Aeroporto Internacional de Brasília', 'BSB', 'Brasília', 'BR', -15.8711, -47.9186, 1067.0),
+('Aeroporto Internacional de Miami', 'MIA', 'Miami', 'US', 25.7933, -80.2906, 2.0),
+('Aeroporto Internacional John F. Kennedy', 'JFK', 'New York', 'US', 40.6397, -73.7789, 4.0),
+('Aeroporto Internacional de Heathrow', 'LHR', 'London', 'GB', 51.4775, -0.4614, 25.0),
+('Aeroporto Internacional Charles de Gaulle', 'CDG', 'Paris', 'FR', 49.0097, 2.5478, 119.0);
